6bf56da374659d9b0cc0e7f56285dcb2ea7a694b
Rishov Sarkar <rishov.sarkar@gmail.com>
2014-02-08 13:16:41 -0500

    Just something fancy.

--- src/com/rsjinc/thefuture/TheFuture.java
+++ src/com/rsjinc/thefuture/TheFuture.java
@@ -3,6 +3,24 @@ package com.rsjinc.thefuture;
 public class TheFuture {
 	
 	public static void main(String[] args) {
-		System.out.print("Initiating voice ID...");
+		System.out.print("Initiating voice ID");
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(".");
+				sleep(1000);
+			}
+			System.out.print("\b\b\b   \b\b\b");
+		}
+		System.out.print("\rDone.              ");
+		System.exit(0);
+	}
+	
+	protected static void sleep(int ms)
+	{
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
