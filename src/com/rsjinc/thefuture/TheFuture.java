e82e158afca7179e9c20c9e50ce0a33d4843be18
Rishov Sarkar <rishov.sarkar@gmail.com>
2014-02-07 20:00:44 -0500

    Removed constructor, added main().

--- src/com/rsjinc/thefuture/TheFuture.java
+++ src/com/rsjinc/thefuture/TheFuture.java
@@ -2,7 +2,7 @@ package com.rsjinc.thefuture;
 
 public class TheFuture {
 	
-	public TheFuture() {
-		
+	public static void main(String[] args) {
+		System.out.print("Initiating voice ID...");
 	}
 }
