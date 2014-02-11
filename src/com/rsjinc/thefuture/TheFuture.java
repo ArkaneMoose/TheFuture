0d4b168a065a88359a498b877b9047b198e8c1d2
belharethsami <belharethsami@gmail.com>
2014-02-11 09:08:12 -0500

    Update TheFuture.java
    added the ln to the print so that it complies with the common print command

--- src/com/rsjinc/thefuture/TheFuture.java
+++ src/com/rsjinc/thefuture/TheFuture.java
@@ -3,15 +3,15 @@ package com.rsjinc.thefuture;
 public class TheFuture {
 	
 	public static void main(String[] args) {
-		System.out.print("Initiating voice ID");
+		System.out.println("Initiating voice ID");
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				System.out.print(".");
+				System.out.println(".");
 				sleep(1000);
 			}
-			System.out.print("\b\b\b   \b\b\b");
+			System.out.println("\b\b\b   \b\b\b");
 		}
-		System.out.print("\rDone.              ");
+		System.out.println("\rDone.              ");
 		System.exit(0);
 	}
 	
